decfafd7115784358452cdbcba38a6f691152dd7
Gregg, Nathan <negregg@mail.wvu.edu>
2019-11-04 13:46:00 -0500

    Improved calculator reset function

--- calculator.py
+++ calculator.py
@@ -12,6 +12,7 @@ Main purpose of program is to test reusability of said library.
 
 
 """
+import sys
 from os import system, name 
 from seng560Calc import *
 
@@ -20,8 +21,8 @@ print(80 * '*')
 print("\nWelcome to the Simple Text Calculator!\n")
 print(80 * '*')
 
-
-while continueProcessing == True:
+#Main Function
+def main ():
     print("\nPlease select from the following options/operations.")
     print("NOTE: This simple calculator can only operate on a max of two numbers.")
     print(80 * '-')
@@ -31,16 +32,23 @@ while continueProcessing == True:
     print("4) Division")
     print("5) Exit Program\n")
     print(80 * '-')
+    
+    try:
+        selection = int(input("Which option would you like to choose? "))
+    except:
+        print("Only 1 through 5 are valid options.  Returning to Main Menu.")
+        resetScreen()
 
-    selection = int(input("Which option would you like to choose? "))
-
+    if selection < 1 or selection > 5:
+        print("Only 1 through 5 are valid options.  Returning to Main Menu.")
+        resetScreen()
+           
     if selection == 5:
-        continueProcessing = False
-        break
+        sys.exit()
     else:
         value1 = int(input("\nEnter the first number: "))
         value2 = int(input("Enter the sencond number: "))
-
+    
     if selection == 1:
         answer = (add(value1,value2))
     elif selection == 2:
@@ -49,14 +57,23 @@ while continueProcessing == True:
         answer = (multiply(value1,value2))
     elif selection == 4:
         answer = (divide(value1,value2))
-
-    print("Answer: " + str(answer))
-    input("\nPress 'Enter' to return to Main Menu: ")
     
+    print("Answer: " + str(answer))
+    resetScreen()
+    
+
+def resetScreen ():
+    input("\nPress 'Enter' to return to Main Menu: ")
     # Clear screen on Windows systems
     if name == 'nt': 
         _ = system('cls') 
-  
+    
     # Clear screen on Linux or OSX
     else: 
         _ = system('clear') 
+
+    # Return to main screen
+    main()
+ 
+# Call main function
+main()
